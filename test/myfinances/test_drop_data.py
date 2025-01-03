2dd0cfa50f821a23cb2107eea963315766e8f1c4
Martin Grader <martingrader@gmail.com>
2025-01-03 17:04:30 +0100

    add another test for dropping data

--- test/myfinances/test_drop_data.py
+++ test/myfinances/test_drop_data.py
@@ -10,29 +10,43 @@ from myfinances.parse_data import Transaction
 
 @pytest.fixture
 def df_before() -> DataFrame[Transaction]:
-    df_before: DataFrame[Transaction] = pd.DataFrame({'a': [1, 2, 3, 2], 'b': [4, 2, 6, 2]})  # type:ignore
+    df_before: DataFrame[Transaction] = pd.DataFrame(
+        {Transaction.Text: ['-1', '1', '2', '3', '2'], 'b': ['-2', '4', '2', '6', '2']}
+    )  # type:ignore
     return df_before
 
 
 @pytest.fixture
 def ser_bool() -> pd.Series:
-    ser_bool: pd.Series = pd.Series([False, True, False, True])
+    ser_bool: pd.Series = pd.Series([False, False, True, False, True])
     return ser_bool
 
 
-def mockreturn(*args, **kwargs) -> pd.Series:
-    ser_bool: pd.Series = pd.Series([False, True, False, True])
+def mock_get_rows_by_string(*args, **kwargs) -> pd.Series:
+    ser_bool: pd.Series = pd.Series([False, False, True, False, True])
     return ser_bool
+
+
+def mock_load_yaml(*args, **kwargs) -> dict:
+    config: dict = {'reason_1': ['3', '2'], 'reason_2': ['-1']}
+    return config
 
 
 def test_drop_data_no_config(df_before) -> None:
-    df_expected: pd.DataFrame = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 2, 6]})
+    df_expected: pd.DataFrame = df_before.iloc[:-1, :]
     df: DataFrame[Transaction] = dt.drop_data(df_before, Path())
     pd.testing.assert_frame_equal(df, df_expected)
 
 
+def test_drop_data(df_before, monkeypatch) -> None:
+    monkeypatch.setattr(dt, 'load_yaml', mock_load_yaml)
+    df_expected: pd.DataFrame = df_before.iloc[1:2, :]
+    df: DataFrame[Transaction] = dt.drop_data(df_before, Path(__file__))
+    pd.testing.assert_frame_equal(df, df_expected)
+
+
 def test_drop_transaction_by_key_and_reason(df_before, monkeypatch, ser_bool) -> None:
-    monkeypatch.setattr(dt, 'get_rows_by_string', mockreturn)
+    monkeypatch.setattr(dt, 'get_rows_by_string', mock_get_rows_by_string)
     df_expected: pd.DataFrame = df_before.iloc[ser_bool[~ser_bool].index.values, :]  # type:ignore
     df: DataFrame[Transaction] = dt.drop_transaction_by_key_and_reason(df_before, '', '')
     pd.testing.assert_frame_equal(df, df_expected)

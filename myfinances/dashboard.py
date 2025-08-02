a95f52db2ace1cbaa1198436b12b8ce6c39b2c62
Martin Grader <martingrader@gmail.com>
2025-08-02 21:55:26 +0200

    add dashboard plot dependencies on date range change

--- myfinances/dashboard.py
+++ myfinances/dashboard.py
@@ -1,5 +1,7 @@
 import dash_bootstrap_components as dbc
+import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from dash import Dash, dcc, dependencies, html
 
 from myfinances.monthly_costs import MonthlyCosts
@@ -40,14 +42,6 @@ class Dashboard:
                     children=[
                         dcc.Graph(
                             id='label_pie',
-                            figure=px.pie(
-                                self.monthly_costs.get_averaged_expenses_by_label()
-                                .drop('Einkommen')
-                                .mul(-1)
-                                .reset_index(),
-                                values='Amount',
-                                names='Label',
-                            ),
                             style={'display': 'inline-block', 'width': '50%'},
                         ),
                         dcc.Graph(
@@ -59,24 +53,36 @@ class Dashboard:
                 html.Div(
                     [
                         html.Label(
-                            f'Available: {monthly_costs.get_averaged_expenses_by_label().sum()}'
+                            id='available_amount',
                         ),
                     ]
                 ),
                 dcc.Graph(
                     id='income_pie',
-                    figure=px.pie(
-                        self.monthly_costs.get_averaged_income(),
-                        values='Amount',
-                        names='Sublabel',
-                    ),
                 ),
             ],
         )
         (  # type: ignore
             self.app.callback(
+                dependencies.Output('available_amount', 'children'),
+                dependencies.Input('begin-dropdown', 'value'),
+                dependencies.Input('end-dropdown', 'value'),
+            )(self.available_amount),
+            self.app.callback(
+                dependencies.Output('label_pie', 'figure'),
+                dependencies.Input('begin-dropdown', 'value'),
+                dependencies.Input('end-dropdown', 'value'),
+            )(self.plot_label_pie),
+            self.app.callback(
+                dependencies.Output('income_pie', 'figure'),
+                dependencies.Input('begin-dropdown', 'value'),
+                dependencies.Input('end-dropdown', 'value'),
+            )(self.plot_income_pie),
+            self.app.callback(
                 dependencies.Output('sublabel_pie', 'figure'),
                 dependencies.Input('label_pie', 'clickData'),
+                dependencies.Input('begin-dropdown', 'value'),
+                dependencies.Input('end-dropdown', 'value'),
             )(self.plot_sublabel_pie),
             self.app.callback(
                 dependencies.Output('expenses-bar', 'figure'),
@@ -85,20 +91,42 @@ class Dashboard:
             )(self.plot_expenses_bar),
         )
 
-    def plot_sublabel_pie(self, clickData):  # noqa: N803
+    def available_amount(self, *_) -> str:
+        return f'Available: {self.monthly_costs.get_averaged_expenses_by_label().sum()}'
+
+    def plot_label_pie(self, *_) -> go.Figure:  # noqa: N803
+        figure: go.Figure = px.pie(
+            self.monthly_costs.get_averaged_expenses_by_label()
+            .drop('Einkommen')
+            .mul(-1)
+            .reset_index(),
+            values='Amount',
+            names='Label',
+        )
+
+        return figure
+
+    def plot_sublabel_pie(self, clickData, *_) -> go.Figure:  # noqa: N803
         label = 'Sonstiges'
         if clickData:
             label: str = clickData['points'][0]['label']
-        figure = px.pie(
+        figure: go.Figure = px.pie(
             self.monthly_costs.get_averaged_expenses_by_sublabel(label).mul(-1).reset_index(),
             values='Amount',
             names='Sublabel',
         )
         return figure
 
-    def plot_expenses_bar(self, begin_dropdown_data, end_dropdown_data):  # noqa: N803
-        # self.monthly_costs.set_transactions(begin_dropdown_data, end_dropdown_data)
-        figure = px.bar(
+    def plot_income_pie(self, *_) -> go.Figure:  # noqa: N803
+        figure: go.Figure = px.pie(
+            self.monthly_costs.get_averaged_income(), values='Amount', names='Sublabel'
+        )
+        return figure
+
+    def plot_expenses_bar(self, begin_dropdown_data, end_dropdown_data) -> go.Figure:  # noqa: N803
+        self.monthly_costs.set_date_to_start(pd.to_datetime(begin_dropdown_data))
+        self.monthly_costs.set_date_to_end(pd.to_datetime(end_dropdown_data))
+        figure: go.Figure = px.bar(
             self.monthly_costs.get_monthly_expenses(),
             x='Date',
             y='Amount',

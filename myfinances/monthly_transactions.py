d23d84aa04403807b1c39ec6da6b63f8f2413955
Martin Grader <martingrader@gmail.com>
2025-09-23 21:29:19 +0200

    refactor error handling

--- myfinances/monthly_transactions.py
+++ myfinances/monthly_transactions.py
@@ -22,40 +22,30 @@ class MonthlyTransactions:
         )
 
     def set_date_to_start(self, date: pd.Timestamp) -> None:
-        valid_date: pd.Timestamp = self.get_date_to_start()
-        try:
-            self._date_to_start = date
-            IntegrityChecker(self).check_start_date()
-        except StartDateError:
-            self._date_to_start = valid_date
-            raise
+        self.set_start_and_end_date(date, self.get_date_to_end())
 
     def set_date_to_end(self, date: pd.Timestamp) -> None:
-        self._check_date_day_matches_split_day(get_next_day(date))
-        self._check_date_limits(date)
-        self._check_start_date_less_end_date(self._date_to_start, date)
-
-        self._date_to_end: pd.Timestamp = date
+        self.set_start_and_end_date(self.get_date_to_start(), date)
 
     def set_start_and_end_date(
         self, date_to_start: pd.Timestamp, date_to_end: pd.Timestamp
     ) -> None:
-        self._check_date_day_matches_split_day(date_to_start)
-        self._check_date_day_matches_split_day(get_next_day(date_to_end))
-        self._check_date_limits(date_to_start)
-        self._check_date_limits(date_to_end)
-        self._check_start_date_less_end_date(date_to_start, date_to_end)
-
-        self._date_to_start: pd.Timestamp = date_to_start
-        self._date_to_end: pd.Timestamp = date_to_end
+        valid_start_date: pd.Timestamp = self.get_date_to_start()
+        valid_end_date: pd.Timestamp = self.get_date_to_end()
+        try:
+            self._date_to_start = date_to_start
+            self._date_to_end = date_to_end
+            DateValidityChecker(self).execute()
+        except DateError:
+            self._date_to_start = valid_start_date
+            self._date_to_end = valid_end_date
+            raise
 
     def set_month_split_day(self, month_split_day: int) -> None:
-        self._set_month_split_day(month_split_day)
+        MonthSplitDateValidityChecker().execute(month_split_day)
+        self._month_split_day: int = month_split_day
         self._reset_start_end_dates()
-
-    def _check_date_limits(self, date: pd.Timestamp) -> None:
-        self._check_date_ge_minimum_date(date)
-        self._check_date_le_max_date(date)
+        DateValidityChecker(self).execute()
 
     def get_transactions(self) -> DataFrame[TransactionLabeled]:
         return self._df.loc[
@@ -127,10 +117,6 @@ class MonthlyTransactions:
     def _set_all_transactions(self, df: DataFrame[TransactionLabeled]) -> None:
         self._df: DataFrame[TransactionLabeled] = df
 
-    def _set_month_split_day(self, month_split_day) -> None:
-        self._check_month_split_day(month_split_day)
-        self._month_split_day: int = month_split_day
-
     def _reset_start_end_dates(self) -> None:
         self._date_to_start: pd.Timestamp = self._min_day_to_start()
         self._date_to_end: pd.Timestamp = self._max_day_to_end()
@@ -169,47 +155,6 @@ class MonthlyTransactions:
             else:
                 return get_previous_day(get_previous_month(day_to_end))
 
-    def _check_date_day_matches_split_day(self, date: pd.Timestamp) -> None:
-        if date.day != self._month_split_day:
-            log.error(
-                f'Day in date ({date}) must equal the month_split_day ({self._month_split_day})'
-            )
-            raise AttributeError
-
-    def _check_date_ge_minimum_date(self, date: pd.Timestamp) -> None:
-        if date < self.get_min_date_to_start():
-            log.error(
-                f'Date set ({date}) must greater or equal',
-                f' the minimum date in data ({self.get_min_date_to_start()})',
-            )
-            raise AttributeError
-
-    def _check_date_le_max_date(self, date: pd.Timestamp) -> None:
-        if date > self.get_max_date_to_end():
-            log.error(
-                f'Date set ({date}) must be less or equal the',
-                f' maximum date in data ({self.get_max_date_to_end()})',
-            )
-            raise AttributeError
-
-    def _check_start_date_less_end_date(
-        self, start_date: pd.Timestamp, end_date: pd.Timestamp
-    ) -> None:
-        if start_date > end_date:
-            log.error(f'Start date ({start_date}) must be less end data ({end_date})')
-            raise AttributeError
-
-    def _check_month_split_day(self, month_split_day: int) -> None:
-        if month_split_day <= 0:
-            log.error(f'Month split day ({month_split_day}) must be greater zero')
-            raise AttributeError
-        if month_split_day >= 28:
-            log.error(f'Month split day ({month_split_day}) must be less 28')
-            raise AttributeError
-        if not isinstance(month_split_day, int):
-            log.error(f'Type of month split day ({month_split_day}) must be integer')
-            raise AttributeError
-
     def drop_costs(self, label: str, sublabel: str) -> None:
         to_drop: pd.Series = (self._df[TransactionLabeled.Label] == label) & (
             self._df[TransactionLabeled.Sublabel] == sublabel
@@ -240,24 +185,26 @@ class MonthlyTransactions:
         self._df = pd.concat([self._df, df_to_add_all_configs])  # type: ignore
 
 
-class StartDateError(Exception):
+class DateError(Exception):
     pass
 
 
-class IntegrityChecker:
+class DateValidityChecker:
     def __init__(self, target: MonthlyTransactions) -> None:
         self.start_date: pd.Timestamp = target.get_date_to_start()
         self.end_date: pd.Timestamp = target.get_date_to_end()
         self.month_split_day: int = target.get_month_split_day()
         self.min_date_to_start: pd.Timestamp = target.get_min_date_to_start()
         self.max_date_to_end: pd.Timestamp = target.get_max_date_to_end()
-        self.error_state: type[Exception] = AttributeError
+        self.error_state: type[Exception] = DateError
 
-    def check_start_date(self) -> None:
-        self.error_state = StartDateError
+    def execute(self) -> None:
         self._check_date_day_matches_split_day(self.start_date)
+        self._check_date_day_matches_split_day(get_next_day(self.end_date))
         self._check_date_ge_minimum_date(self.start_date)
+        self._check_date_ge_minimum_date(self.end_date)
         self._check_date_le_max_date(self.start_date)
+        self._check_date_le_max_date(self.end_date)
         self._check_start_date_less_end_date(self.start_date, self.end_date)
 
     def _check_date_day_matches_split_day(self, start_date: pd.Timestamp) -> None:
@@ -289,3 +236,19 @@ class IntegrityChecker:
         if start_date > end_date:
             log.error(f'Start date ({start_date}) must be less end data ({end_date})')
             raise self.error_state
+
+
+class MonthSplitDateValidityChecker:
+    min_split_day: int = 1
+    max_split_day: int = 27
+
+    def execute(self, month_split_day: int) -> None:
+        if month_split_day < self.min_split_day:
+            log.error(f'Month split day ({month_split_day}) must be greater zero')
+            raise DateError
+        if month_split_day > self.max_split_day:
+            log.error(f'Month split day ({month_split_day}) must be less 28')
+            raise DateError
+        if not isinstance(month_split_day, int):
+            log.error(f'Type of month split day ({month_split_day}) must be integer')
+            raise DateError
--- test/myfinances/test_monthly_transactions_error_handling.py
+++ test/myfinances/test_monthly_transactions_error_handling.py
@@ -4,7 +4,12 @@ from pandas._libs import NaTType
 from pandera.typing import DataFrame
 
 from myfinances.label_data import TransactionLabeled
-from myfinances.monthly_transactions import MonthlyTransactions, StartDateError
+from myfinances.monthly_transactions import (
+    DateError,
+    DateValidityChecker,
+    MonthlyTransactions,
+    MonthSplitDateValidityChecker,
+)
 from myfinances.utils import get_next_day, get_previous_day, get_previous_month
 
 
@@ -64,21 +69,21 @@ def date_to_end_expected() -> pd.Timestamp | NaTType | None:
 
 def test_set_date_to_start_below_data(monthly_transactions, date_to_start_expected) -> None:
     date_to_start_valid: pd.Timestamp = monthly_transactions.get_date_to_start()
-    with pytest.raises(StartDateError):
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_start(get_previous_month(date_to_start_expected))
     assert monthly_transactions.get_date_to_start() == date_to_start_valid
 
 
 def test_set_date_to_start_above_data(monthly_transactions, date_to_end_expected) -> None:
     date_to_start_valid: pd.Timestamp = monthly_transactions.get_date_to_start()
-    with pytest.raises(StartDateError):
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_start(get_next_day(date_to_end_expected))
     assert monthly_transactions.get_date_to_start() == date_to_start_valid
 
 
 def test_set_date_to_start_wrong_day(monthly_transactions, date_to_start_expected) -> None:
     date_to_start_valid: pd.Timestamp = monthly_transactions.get_date_to_start()
-    with pytest.raises(StartDateError):
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_start(get_next_day(date_to_start_expected))
     assert monthly_transactions.get_date_to_start() == date_to_start_valid
 
@@ -86,33 +91,64 @@ def test_set_date_to_start_wrong_day(monthly_transactions, date_to_start_expecte
 def test_set_date_to_start_ge_date_to_end(monthly_transactions) -> None:
     date_to_start_valid: pd.Timestamp = monthly_transactions.get_date_to_start()
     monthly_transactions.set_date_to_end(pd.Timestamp(year=2024, month=2, day=29))
-    with pytest.raises(StartDateError):
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_start(pd.Timestamp(year=2024, month=3, day=1))
     assert monthly_transactions.get_date_to_start() == date_to_start_valid
 
 
 def test_set_end_to_start_below_data(monthly_transactions, date_to_start_expected) -> None:
-    with pytest.raises(AttributeError):
+    date_to_end_valid: pd.Timestamp = monthly_transactions.get_date_to_end()
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_end(get_previous_month(date_to_start_expected))
+    assert monthly_transactions.get_date_to_end() == date_to_end_valid
 
 
 def test_set_date_to_end_above_data(monthly_transactions, date_to_end_expected) -> None:
-    with pytest.raises(AttributeError):
+    date_to_end_valid: pd.Timestamp = monthly_transactions.get_date_to_end()
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_end(get_next_day(date_to_end_expected))
+    assert monthly_transactions.get_date_to_end() == date_to_end_valid
 
 
 def test_set_date_to_end_wrong_day(monthly_transactions, date_to_end_expected) -> None:
-    with pytest.raises(AttributeError):
+    date_to_end_valid: pd.Timestamp = monthly_transactions.get_date_to_end()
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_end(get_previous_day(date_to_end_expected))
+    assert monthly_transactions.get_date_to_end() == date_to_end_valid
 
 
 def test_set_date_to_end_le_date_to_start(monthly_transactions) -> None:
+    date_to_end_valid: pd.Timestamp = monthly_transactions.get_date_to_end()
     monthly_transactions.set_date_to_start(pd.Timestamp(year=2024, month=3, day=1))
-    with pytest.raises(AttributeError):
+    with pytest.raises(DateError):
         monthly_transactions.set_date_to_end(pd.Timestamp(year=2024, month=2, day=29))
+    assert monthly_transactions.get_date_to_end() == date_to_end_valid
+
+
+class MonthlyTransactionsMock:
+    def get_date_to_start(self) -> pd.Timestamp | NaTType | None:
+        return pd.Timestamp(year=2024, month=2, day=1)
+
+    def get_date_to_end(self) -> pd.Timestamp | NaTType | None:
+        return pd.Timestamp(year=2024, month=3, day=31)
+
+    def get_month_split_day(self) -> int:
+        return 2
+
+    def get_min_date_to_start(self) -> pd.Timestamp | NaTType | None:
+        return pd.Timestamp(year=2024, month=2, day=1)
+
+    def get_max_date_to_end(self) -> pd.Timestamp | NaTType | None:
+        return pd.Timestamp(year=2024, month=3, day=31)
+
+
+def test_date_validity_checker() -> None:
+    target = MonthlyTransactionsMock()
+    with pytest.raises(DateError):
+        DateValidityChecker(target).execute()  # type: ignore
 
 
 @pytest.mark.parametrize('month_split_day', [0, 29, 2.3])
-def test_split_day_wrong(df_test, month_split_day) -> None:
-    with pytest.raises(AttributeError):
-        MonthlyTransactions(df_test, month_split_day)
+def test_split_day_wrong(month_split_day) -> None:
+    with pytest.raises(DateError):
+        MonthSplitDateValidityChecker().execute(month_split_day)
